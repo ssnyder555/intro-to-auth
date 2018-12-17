f88508275f0a713057dffe972471793929e3fa5b
ssnyder555 <ssnyder555@live.com>
2018-12-17 14:25:03 -0600

    added user auth

--- app.js
+++ app.js
@@ -1,17 +1,86 @@
-let express = require("express");
-let mongoose = require("mongoose");
+let express               = require("express"),
+    mongoose              = require("mongoose"),
+    passport              = require("passport"),
+    bodyParser            = require("body-parser"),
+    User                  = require("./models/user"),
+    LocalStrategy         = require("passport-local"),
+    passportLocalMongoose = require("passport-local-mongoose");
+
+
+
 mongoose.connect("mongodb://localhost/auth_demo_app",{ useNewUrlParser: true });
+
 let app = express();
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(require("express-session")({
+  secret: "Frankie is the cutest dog in the world",
+  resave: false,
+  saveUninitialized: false
+}));
 app.set("view engine", "ejs");
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+passport.use(new LocalStrategy(User.authenticate()));
+// +=============+
+// Routes
+// ===============
 
 
 app.get("/", function(req, res){
   res.render("home")
 });
-app.get("/secret", function(req, res){
+app.get("/secret", isLoggedIn, function(req, res){
   res.render("secret");
 });
 
+// Sign up Form:
+app.get("/register", function(req, res){
+  res.render("register")
+})
+
+// handling user sign up
+app.post("/register", function(req, res){
+  req.body.username
+  req.body.password
+  User.register(new User({username: req.body.username}), req.body.password, function(err, user){
+    if(err){
+      console.log(err);
+      return res.render("register");
+    }
+    passport.authenticate("local")(req, res, function(){
+      res.redirect("/secret");
+    })
+  })
+});
+
+// Login Routes
+// Render Login form
+app.get("/login", function(req, res){
+  res.render("login");
+});
+
+// Login Logic
+// Middleware
+app.post("/login", passport.authenticate("local", {
+  successRedirect: "/secret",
+  failureRedirect: "/login"
+}), function(req, res){
+
+});
+
+app.get("/logout", function(req, res){
+  req.logout();
+  res.redirect("/");
+});
+
+function isLoggedIn(req, res, next){
+  if(req.isAuthenticated()){
+    return next();
+  }
+  res.redirect("/login");
+}
 
 app.listen(3000, function(){
   console.log("server is started");
---
+++ models/user.js
@@ -0,0 +1,11 @@
+let mongoose = require("mongoose");
+let passportLocalMongoose = require("passport-local-mongoose")
+
+const UserSchema = new mongoose.Schema({
+  username: String,
+  password: String
+});
+
+UserSchema.plugin(passportLocalMongoose);
+
+module.exports = mongoose.model("User", UserSchema);
